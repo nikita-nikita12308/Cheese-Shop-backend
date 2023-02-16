a7946cd4258e8d8f8f82ce0ffbccb663b2048860
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-02-16 20:20:12 +0200

    day 2

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const http = require('http')
 const url = require('url')
+const replaceTemplate = require('./modules.js/replaceTemplate')
 
+const slugify = require('slugify')
 
 //Files
 
@@ -29,19 +31,6 @@ const url = require('url')
 
 
 //SERVER
-const replaceTemplate = (temp, product) => {
-	let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
-	output = output.replace(/{%IMAGE%}/g, product.image)
-	output = output.replace(/{%PRICE%}/g, product.price)
-	output = output.replace(/{%FROM%}/g, product.from)
-	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-	output = output.replace(/{%QUANTITY%}/g, product.quantity)
-	output = output.replace(/{%DESCRIPTION%}/g, product.description)
-	output = output.replace(/{%ID%}/g, product.id)
-
-	if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-	return output;
-}
 
 
 // templates
@@ -53,11 +42,16 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
 
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true}))
+console.log(slugs)
+
 const server = http.createServer((req,res) => {
-	const pathName = req.url
+ 	
+	const { query, pathname} = url.parse(req.url, true)
+
 
 	// Overview Page
-	if( pathName === '/' || pathName === '/overview') {
+	if( pathname === '/' || pathname === '/overview') {
 		res.writeHead(200, {'Content-type': 'text/html'})
 
 		const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
@@ -65,11 +59,14 @@ const server = http.createServer((req,res) => {
 		res.end(output)
 
 	// Product Page
-	}else if(pathName === '/product'){
-		res.end('This is the product')
+	}else if(pathname === '/product'){
+		res.writeHead(200, {'Content-type': 'text/html'})
+		const product = dataObj[query.id]
+		const output = replaceTemplate(tempProduct, product)
+		res.end(output)
 
 	// API	
-	}else if(pathName === '/api'){
+	}else if(pathname === '/api'){
 		res.writeHead(200, { 'Content-type': 'application/json'})
 		res.end(data);
 
@@ -84,7 +81,7 @@ const server = http.createServer((req,res) => {
 })
 
 server.listen(8000, '127.0.0.1', () => { 
-	console.log('Hello brother')
+	console.log('Listening to req at port 8000')
 })
 
 
