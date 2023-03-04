df8f038e066acf0ce8e02ca2867f7492c53f50da
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-03-04 23:13:48 +0200

    mongo db

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -6,6 +6,10 @@ const router = express.Router();
 //router.param('id', tourController.checkID)
 
 router
+  .route('/tour-stats')
+  .get(tourController.getTourStats)
+
+router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours)
 
