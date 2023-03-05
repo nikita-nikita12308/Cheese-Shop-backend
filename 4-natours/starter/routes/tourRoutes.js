4ffd91e5d4c22101145db21ca2d6f735595c8cd9
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-03-05 21:32:40 +0200

    day 1 (again)

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -10,6 +10,10 @@ router
   .get(tourController.getTourStats)
 
 router
+  .route('/monthly-plan/:year')
+  .get(tourController.getMonthlyPlan)
+
+router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours)
 
