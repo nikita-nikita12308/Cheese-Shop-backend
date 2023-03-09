5c4efb349cec997434ff5e0d9d002395d5cd0d29
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-03-09 22:00:50 +0200

    day 4 upd

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -3,8 +3,12 @@ const mongoose = require('mongoose');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-	console.log('signup')
-	const newUser = await User.create(req.body);
+	const newUser = await User.create({
+		name: req.body.name,
+		email: req.body.email,
+		password: req.body.password,
+		passwordConfirm: req.body.passwordConfirm
+	});
 	res.status(201).json({
 		status: 'success',
 		data: {
