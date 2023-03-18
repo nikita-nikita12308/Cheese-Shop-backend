b5aec5118d927d5d398b74268e21e54722b276f8
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-03-18 21:22:31 +0200

    fixed small bug

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -19,7 +19,7 @@ handleValidationErrorDB = err => {
 }
 
 
-const handleJWTError = err => new AppError('Invalid token. Please log in again')
+const handleJWTError = () => new AppError('Invalid token. Please log in again', 401)
 
 const sendErrorDev = (err, res) => {
 	res.status(err.statusCode).json({
@@ -57,7 +57,7 @@ module.exports = (err, req, res, next) => {
 		sendErrorDev(err, res)
 	} else if (process.env.NODE_ENV === 'production'){
 		let error = Object.create(err);
-
+		console.log(error.name)
 		if(error.name === 'CastError') error = handleCastErrorDB(error);
 		if(error.code === 11000) error = handleDuplicateFieldsDB(error);
 		if(error.name === 'ValidationError') error = handleValidationErrorDB(error);
