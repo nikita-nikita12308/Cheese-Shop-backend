231dd551c166c7d86e84ccae7606e0bec60bc5a1
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-04-30 01:10:11 +0300

    day 1
    nodemailer

---
+++ 4-natours/starter/utils/email.js
@@ -0,0 +1,26 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async options => {
+  // 1) transporter
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD
+        }
+        // Activate in gmail 'less secure app' option
+    });
+  // 2) define email options
+    const mailOptions = {
+        from: 'Nikita <ndatskiy@gmail.com>',
+        to: options.email,
+        subject: options.subject,
+        text: options.message
+        // html:
+    };
+  // 3) Actually send email with nodemailer
+    await transporter.sendMail(mailOptions)
+};
+
+module.exports = sendEmail;
