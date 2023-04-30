ee38c6b67d45fe169c2a59d9baba51c2d9e23f9c
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-04-30 16:03:02 +0300

    day2
    reset password

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
@@ -112,7 +113,7 @@ exports.forgotPassword = catchAsync( async(req, res, next) => {
 	const resetToken = user.createPasswordResetToken();
 	await user.save({ validateBeforeSave: false });
 	// 3) Send it to users email
-    const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword${resetToken}`;
+    const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
     const message = `Forgot your password? Submit a PATCH request with your new password and
      passwordConfirm to: ${resetURL}. \nIf you didn't forget your password , please ignore this email! `;
 
@@ -137,6 +138,28 @@ exports.forgotPassword = catchAsync( async(req, res, next) => {
 
 });
 
-exports.resetPassword = (req, res, next) => {
+exports.resetPassword = catchAsync(async (req, res, next) => {
+	// 1) get user based on the token
+	const hashedToken = crypto.createHash('sha256')
+		.update(req.params.token)
+		.digest('hex');
 
-};
+	const user = await User.findOne({ passwordResetToken: hashedToken, passwordResetExpires:  {$gt :Date.now() } });
+
+	// 2) if token has not expired, and there is user , set the new password
+	if(!user) return next(new AppError('Token is invalid or have expired', 400));
+	user.password = req.body.password;
+	user.passwordConfirm = req.body.passwordConfirm;
+	user.passwordResetToken = undefined;
+	user.passwordResetExpires = undefined;
+	await user.save();
+
+	// 3) Update the changePasswordAt property for the user
+	// 4) Log the user in , send JWT
+	const token = signToken(user._id);
+
+	res.status(200).json({
+		status: 'success',
+		token
+	});
+});
