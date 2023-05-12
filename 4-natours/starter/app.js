6844d6b2e7109bea3bb2002994e5eff5f768e3ca
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-05-12 16:49:43 +0300

    day 6

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globbalErrorHandler = require('./controllers/errorController');
@@ -9,12 +10,17 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 console.log(process.env.NODE_ENV);
 if(process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
 
+// Limit requests from  same API
 const limiter = rateLimit({
 	max: 100,
 	windowMs: 60 * 60 * 1000,
@@ -22,9 +28,15 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
+
+// Body parser, reading data from body into req.body
+app.use(express.json({	limit: '10kb'}));
+
+// Serving static files
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
 	req.requestTime = new Date().toISOString();
 	next();
