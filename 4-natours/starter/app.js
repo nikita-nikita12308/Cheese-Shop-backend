9422e4932cb6b70178e7e14a9a72bc9537bbf911
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-05-12 16:07:37 +0300

    day 6
    rate limit feature

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,27 +1,34 @@
-
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
-const AppError = require('./utils/appError')
-const globbalErrorHandler = require('./controllers/errorController')
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const AppError = require('./utils/appError');
+const globbalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 // 1) MIDDLEWARES
-console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV);
 if(process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
 
+const limiter = rateLimit({
+	max: 100,
+	windowMs: 60 * 60 * 1000,
+	message: 'Too many request from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
 	req.requestTime = new Date().toISOString();
 	next();
-})
+});
 
 // 3) ROUTES
 
@@ -30,7 +37,7 @@ app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
 	next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
-})
+});
 
 app.use(globbalErrorHandler)
 
