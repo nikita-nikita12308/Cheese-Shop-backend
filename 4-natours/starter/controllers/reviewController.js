5015f6d388206c997a08fab9ef0cc8b8102cc6d8
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-05-16 22:39:28 +0300

    day 10

--- 4-natours/starter/controllers/reviewController.js
+++ 4-natours/starter/controllers/reviewController.js
@@ -15,6 +15,10 @@ exports.getAllReviews = catchAsync(async function (req, res, next) {
 });
 
 exports.createReview = catchAsync(async function(req, res, next){
+    // Allow nested routes
+    // allow nested routes
+   if(!req.body.tour) req.body.tour = req.params.tourId;
+   if(!req.body.user) req.body.user = req.user.id;
    const newReview = await Review.create(req.body);
 
     res.status(201).json({
--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
+const reviewController = require('./../controllers/reviewController');
 
 const router = express.Router();
 
@@ -30,5 +31,13 @@ router
   .delete(authController.protect,
       authController.restrictTo('admin') ,
       tourController.deleteTour);
+
+router
+    .route('/:tourId/reviews')
+    .post(
+        authController.protect,
+        authController.restrictTo('user'),
+        reviewController.createReview
+    );
 
 module.exports = router;
