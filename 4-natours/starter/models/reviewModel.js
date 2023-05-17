040d76654666d6fa9acde6d61c0f99c5125f7880
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-05-18 00:11:45 +0300

    day  10

--- 4-natours/starter/models/reviewModel.js
+++ 4-natours/starter/models/reviewModel.js
@@ -51,6 +51,27 @@ reviewSchema.pre(/^find/, function(next){
     next();
 });
 
+reviewSchema.statics.calcAverageRatings = async function(tourId) {
+    const stats = await this.aggregate([
+        {
+            $match: { tour: tour}
+        },
+        {
+            $group: {
+                _id: '$tour',
+                nRating: { $sum: 1 },
+                avgRating: { $avg: '$rating' }
+            }
+        }
+    ]);
+    console.log(stats);
+};
+
+reviewSchema.pre('save', function(next){
+    // this points to current review
+    Review.calcAverageRatings(this.tour)
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
