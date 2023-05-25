9d7b9ac12aebb502075b211357fee1d36c7c0f9c
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-05-25 15:55:26 +0300

    day 2

---
+++ 4-natours/starter/public/js/mapbox.js
@@ -0,0 +1,45 @@
+
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+console.log(locations);
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibmlraXRvbjIyODkiLCJhIjoiY2xpMzMyNnFyMHM3dDNsbHBxbnh1bnF4eSJ9.3u00-ST0fxWR8p9USL-5XA';
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/nikiton2289/cli33r98r00k201quha6od1x7',
+    scrollZoom: false
+    //    center: [30.630167, 50.461622],
+    //     zoom: 10,
+    //     interactive: false
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+    // Marker
+   const el = document.createElement('div');
+   el.className = 'marker';
+
+   new mapboxgl.Marker({
+       element: el,
+       anchor: 'bottom'
+   }).setLngLat(loc.coordinates).addTo(map);
+
+   // Pop Up
+    new mapboxgl.Popup({
+        offset: 30
+    }).setLngLat(loc.coordinates)
+        .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+        .addTo(map);
+
+   bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+    padding: {
+        top: 200,
+        bottom: 200,
+        left: 100,
+        right: 100
+    }
+});
--- 4-natours/starter/routes/viewRoutes.js
+++ 4-natours/starter/routes/viewRoutes.js
@@ -3,6 +3,24 @@ const viewsController = require('../controllers/viewsController');
 
 const router = express.Router();
 
+const CSP = 'Content-Security-Policy';
+const POLICY =
+    "default-src 'self' https://*.mapbox.com ;" +
+    "base-uri 'self';block-all-mixed-content;" +
+    "font-src 'self' https: data:;" +
+    "frame-ancestors 'self';" +
+    "img-src http://localhost:8000 'self' blob: data:;" +
+    "object-src 'none';" +
+    "script-src https: cdn.jsdelivr.net cdnjs.cloudflare.com api.mapbox.com 'self' blob: ;" +
+    "script-src-attr 'none';" +
+    "style-src 'self' https: 'unsafe-inline';" +
+    'upgrade-insecure-requests;';
+
+router.use((req, res, next) => {
+    res.setHeader(CSP, POLICY);
+    next();
+});
+
 router.get('/', viewsController.getOverview);
 router.get('/tours/:tourName', viewsController.getTour);
 
