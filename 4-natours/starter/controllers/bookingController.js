9f1a9b2fe4ec8c168d30c26daf621bcd8f164bb1
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-05-27 23:51:38 +0300

    day 4

--- 4-natours/starter/controllers/bookingController.js
+++ 4-natours/starter/controllers/bookingController.js
@@ -47,3 +47,9 @@ exports.createBookingCheckOut = catchAsync(async (req, res, next) => {
     res.redirect(req.originalUrl.split('?')[0])
 
 });
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
--- 4-natours/starter/routes/bookingRoutes.js
+++ 4-natours/starter/routes/bookingRoutes.js
@@ -5,9 +5,22 @@ const authController = require('./../controllers/authController');
 const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
+router.use(authController.protect);
 
 router.get('/checkout-session/:tourId',
-    authController.protect,
     bookingController.getCheckOutSession);
+
+router.use(authController.restrictTo('admin', 'lead-guide'));
+
+router
+    .route('/')
+    .get(bookingController.getAllBookings)
+    .post(bookingController.createBooking);
+
+router
+    .route('/:id')
+    .get(bookingController.getBooking)
+    .patch(bookingController.updateBooking)
+    .delete(bookingController.deleteBooking);
 
 module.exports = router;
