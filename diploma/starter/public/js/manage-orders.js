36bad0219f24b891a1811e86f359547253c765ea
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-06-14 17:34:06 +0300

    admin manage orders functional
    without delete operation

--- diploma/starter/public/js/manage-orders.js
+++ diploma/starter/public/js/manage-orders.js
@@ -1,39 +1,71 @@
 function toggleEditing(orderId) {
-  var productName = document.getElementById(`productName_${orderId}`);
-  var userName = document.getElementById(`userName_${orderId}`);
-  var userEmail = document.getElementById(`userEmail_${orderId}`);
-  var price = document.getElementById(`price_${orderId}`);
-  var paid = document.getElementById(`paid_${orderId}`);
-  var createdAt = document.getElementById(`createdAt_${orderId}`);
 
-  if (productName.getElementsByTagName("input").length === 0) {
+  let price = document.getElementById(`price_${orderId}`);
+  let paid = document.getElementById(`paid_${orderId}`);
+  let createdAt = document.getElementById(`createdAt_${orderId}`);
+  let userId = document.getElementById(`userId_${orderId}`);
+  let productId = document.getElementById(`productId_${orderId}`);
+  if (price.getElementsByTagName("input").length === 0) {
     // Enter editing mode
-    var originalProductName = productName.innerHTML;
-    var originalUserName = userName.innerHTML;
-    var originalUserEmail = userEmail.innerHTML;
-    var originalPrice = price.innerHTML;
-    var originalPaid = paid.innerHTML;
-    var originalCreatedAt = createdAt.innerHTML;
 
-    productName.innerHTML = '<input type="text" value="' + originalProductName + '" />';
-    userName.innerHTML = '<input type="text" value="' + originalUserName + '" />';
-    userEmail.innerHTML = '<input type="text" value="' + originalUserEmail + '" />';
+    let originalPrice = price.innerHTML;
+    let originalPaid = paid.innerHTML;
+    let originalCreatedAt = createdAt.innerHTML;
+    let originalUserId = userId.innerHTML;
+    let originalProductId = productId.innerHTML;
+
     price.innerHTML = '<input type="text" value="' + originalPrice + '" />';
     paid.innerHTML = '<input type="text" value="' + originalPaid + '" />';
+    userId.innerHTML = '<input type="text" value="' + originalUserId + '" />';
+    productId.innerHTML = '<input type="text" value="' + originalProductId + '" />';
+
   } else {
     // Exit editing mode
-    var updatedProductName = productName.getElementsByTagName("input")[0].value;
-    var updatedUserName = userName.getElementsByTagName("input")[0].value;
-    var updatedUserEmail = userEmail.getElementsByTagName("input")[0].value;
-    var updatedPrice = price.getElementsByTagName("input")[0].value;
-    var updatedPaid = paid.getElementsByTagName("input")[0].value;
 
+    const updatedPrice = price.getElementsByTagName("input")[0].value;
+    const updatedPaid = paid.getElementsByTagName("input")[0].value;
+    const updatedUserId = userId.getElementsByTagName("input")[0].value;
+    const updatedProductId = productId.getElementsByTagName("input")[0].value;
 
-    productName.innerHTML = updatedProductName;
-    userName.innerHTML = updatedUserName;
-    userEmail.innerHTML = updatedUserEmail;
     price.innerHTML = updatedPrice;
     paid.innerHTML = updatedPaid;
-
+    userId.innerHTML = updatedUserId;
+    productId.innerHTML = updatedProductId;
   }
 }
+
+const updateOrder = async (orderId) => {
+  try{
+    const price = document.getElementById(`price_${orderId}`).innerHTML;
+    const paid = document.getElementById(`paid_${orderId}`).innerHTML;
+    const product = document.getElementById(`productId_${orderId}`).innerHTML;
+    const user = document.getElementById(`userId_${orderId}`).innerHTML;
+    console.log('Product => ' + product);
+    console.log('User => ' + user);
+
+    const updateRes = await axios({
+      method: 'PATCH',
+      url: `http://127.0.0.1:8000/api/v1/booking/${orderId}`,
+      data: {
+        product,
+        user,
+        price,
+        paid
+      }
+    });
+
+    if (updateRes.data.status === 'success') {
+      showAlert('success', `Updated successfully`);
+      window.setTimeout(() => {
+        location.reload(true)
+      }, 1500);
+
+    }
+  }catch(e){
+    showAlert('error', e.message)
+  }
+};
+
+const deleteOrder = (orderId) => {
+
+};
